0c538a4d6bfba50822ba4ce816ace98f7fdde799
Achilles45 <achillesusuoyibo7@gmail.com>
2020-07-06 21:18:46 -0700

    Second commit of the application

--- src/views/Withdrawal.vue
+++ src/views/Withdrawal.vue
@@ -36,7 +36,7 @@
              <small>You can now make request to make withdrawal into your local bank when your investment has reached maturity</small>
              <hr>
               <div v-if="verifyuser == 'false'" class="red">
-                  You can't make withdrawals because your account or payment has not been verified. Please contact admin or call <a href="tel: 08070964131"> 08070964131</a>
+                  You can't make withdrawals because your account or payment has not been verified. Please contact admin or call <a href="tel: 07026035834"> 07026035834</a>
               </div>
                <div class="row">
                    <div class="col-md-8">
